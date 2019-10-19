17508f52788a23f2133f0541bb332bf39d8874fe
xMdb <masteroflots@gmail.com>
2019-10-19 16:51:03 +1000

    +main.js, smooth scrolling

---
+++ dev/assets/js/main.js
@@ -0,0 +1,20 @@
+// Smooth Scrolling
+$('nav ul li a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 50
+      },
+      800
+    );
+  }
+});
+
+// Loader
+$(window).on("load", function () {
+  $(".loaderContainer").fadeOut("slow");
+});
