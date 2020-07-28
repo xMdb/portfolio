d2ee4a809fca455ec01467eb62a8bdf980c97eaa
xMdb <46137770+xMdb@users.noreply.github.com>
2020-07-28 21:31:05 +1000

    Working on redesign in /dev/

---
+++ dev/assets/js/main.js
@@ -0,0 +1,25 @@
+// Smooth Scrolling
+$('nav, header, nav ul li a').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 50
+      },
+      800
+    );
+  }
+});
+
+$("a[href='#']").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
+
+// Loader
+$(window).on("load", function () {
+  $(".loaderContainer").fadeOut("slow");
+});
