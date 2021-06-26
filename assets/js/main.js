c69f6d8c12448e2b3e4957319c0ac4c8f7c1eadc
Matthew G <46137770+xMdb@users.noreply.github.com>
2021-06-26 23:18:36 +1000

    🚧 Re-write entire website again, new fresher look

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,17 @@
-// Smooth Scrolling
-$('nav, header, #splashFeature ul li a').on('click', function (event) {
-  if (this.hash !== '') {
-    event.preventDefault();
+$(document).ready(function () {
+  $("#about").hide();
+  
+  $("#seemore a").click(function () {
+    $("#profile").fadeOut(1000);
+    setTimeout(() => {
+      $("#about").fadeIn();
+    }, 1000);
+  });
 
-    const hash = this.hash;
-
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top - 0
-      },
-      800
-    );
-  }
-});
-
-$("a[href='#']").click(function () {
-  $("html, body").animate({ scrollTop: 0 }, "slow");
-  return false;
-});
-
-// Loader
-$(window).on("load", function () {
-  $(".loaderContainer").fadeOut("slow");
+  $("#back a").click(function () {
+    $("#about").fadeOut(1000);
+    setTimeout(() => {
+      $("#profile").fadeIn();
+    }, 1000);
+  });
 });
