7a8cccb2cba48fa2a3fe14b2825717719db45c0a
Matthew G <46137770+xMdb@users.noreply.github.com>
2021-08-21 01:25:28 +1000

    🔥 No more evil _blank

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,35 @@
 $(document).ready(function () {
+   // Hide other page on load
+   $('#portfolio').hide();
 
-  // Hide other page on load
-  $("#portfolio").hide();
+   // Show other page on learn more click
+   $('#learnMore a').click(function () {
+      $('#profile').fadeOut(1000);
+      setTimeout(() => {
+         $('#portfolio').fadeIn();
+      }, 1000);
+      return false;
+   });
 
-  // Show other page on learn more click
-  $("#learnMore a").click(function () {
-    $("#profile").fadeOut(1000);
-    setTimeout(() => {
-      $("#portfolio").fadeIn();
-    }, 1000);
-    return false;
-  });
+   // Show profile on back arrow click
+   $('.back').click(function () {
+      $('#portfolio').fadeOut(1000);
+      setTimeout(() => {
+         $('#profile').fadeIn();
+      }, 1000);
+      return false;
+   });
 
-  // Show profile on back arrow click
-  $(".back").click(function () {
-    $("#portfolio").fadeOut(1000);
-    setTimeout(() => {
-      $("#profile").fadeIn();
-    }, 1000);
-    return false;
-  });
-
-  $("#tabs a").click(function () {
-    // Check for active
-    $("#tabs li").removeClass("active");
-    $(this).parent().addClass("active");
-    // Display active tab
-    const currentTab = $(this).attr("href");
-    $("#tabsContent div").fadeOut(100);
-    setTimeout(() => {
-      $(currentTab).fadeIn();
-    }, 100);
-
-    return false;
-  });
-
+   $('#tabs a').click(function () {
+      // Check for active
+      $('#tabs li').removeClass('active');
+      $(this).parent().addClass('active');
+      // Display active tab
+      const currentTab = $(this).attr('href');
+      $('#tabsContent div').fadeOut(100);
+      setTimeout(() => {
+         $(currentTab).fadeIn();
+      }, 100);
+      return false;
+   });
 });
