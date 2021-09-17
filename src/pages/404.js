de138682ff8d3ac7714356dd2eb444c520058b9f
Matthew G <46137770+xMdb@users.noreply.github.com>
2021-09-17 23:53:43 +1000

    🎨 Add hello world

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function NotFound() {
+   return (
+      <>
+         <center>
+            <h1>404 | Not Found</h1>
+         </center>
+      </>
+   );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+   return (
+      <>
+         <center>
+            <h1>sup world</h1>
+         </center>
+      </>
+   );
+}
