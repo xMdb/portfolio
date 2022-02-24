fafc89886349b8916d76c74cd55b3748763b6df6
Matt <46137770+xMdb@users.noreply.github.com>
2022-02-24 18:17:30 +1000

    feat: add additional skills

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,7 +1,52 @@
 import { Text, Flex, Box, chakra, Container, SimpleGrid, HStack, VStack } from '@chakra-ui/react';
-import { RiCustomerService2Fill, RiVideoAddLine, RiSoundModuleFill, RiTerminalBoxLine } from 'react-icons/ri';
+import {
+	RiVideoAddLine,
+	RiTerminalBoxLine,
+	RiAdminLine,
+	RiCodeSSlashLine,
+	RiCustomerService2Line,
+	RiSoundModuleLine,
+} from 'react-icons/ri';
 
 export const Experience = () => {
+	const skills = [
+		{
+			id: 0,
+			icon: <RiCustomerService2Line />,
+			title: 'Customer Service',
+			text: 'I assist customers day-to-day by providing them with exceptional customer service.',
+		},
+		{
+			id: 1,
+			icon: <RiVideoAddLine />,
+			title: 'Digital Creator',
+			text: 'I create various digital works such as videos, designs, and artwork in Figma, Adobe Illustrator, Adobe Photoshop, and Adobe Premiere Pro.',
+		},
+		{
+			id: 2,
+			icon: <RiSoundModuleLine />,
+			title: 'Tech Crew',
+			text: 'I work with my fellow peers at school to create outstanding musical performances and events.',
+		},
+		{
+			id: 3,
+			icon: <RiCodeSSlashLine />,
+			title: 'Full-Stack Developer',
+			text: 'I regularly develop open-source projects using technologies such as Node.js, React, and TypeScript.',
+		},
+		{
+			id: 4,
+			icon: <RiAdminLine />,
+			title: 'System Administration',
+			text: 'I have setup my own Linux homelab server using a Raspberry Pi to test out various applications and configurations in regards to networking and DevOps.',
+		},
+		{
+			id: 5,
+			icon: <RiTerminalBoxLine />,
+			title: 'DevOps',
+			text: 'I regularly test and deploy my applications and custom services to AWS servers by using tools like GitHub and Docker.',
+		},
+	];
 	return (
 		<Box mt={['100vw', '10vw']} id="experience">
 			<Flex flexDirection="column" p={30} textAlign="center" justifyContent="center" alignItems="center">
@@ -14,20 +59,6 @@ export const Experience = () => {
 				<Text fontSize={{ base: 'sm', md: 'md', lg: 'xl' }} fontFamily="heading" color="teal.100" pb={5}>
 					My Skills
 				</Text>
-				{/* <Flex flexDirection="row" justifyContent="center" alignItems="center">
-					<Link href="#">
-						<Icon as={RiCustomerService2Fill} width={100} height={75} m={10} />
-					</Link>
-					<Link href="#">
-						<Icon as={RiSoundModuleFill} width={100} height={75} m={10} />
-					</Link>
-					<Link href="#">
-						<Icon as={RiTerminalBoxLine} width={100} height={75} m={10} />
-					</Link>
-					<Link href="#">
-						<Icon as={RiVideoAddLine} width={100} height={75} m={10} />
-					</Link>
-				</Flex> */}
 				<Container maxW={'6xl'} mt={10} textAlign="left">
 					<SimpleGrid columns={{ base: 1, md: 2, lg: 2 }} spacing={10}>
 						{skills.map((feature) => (
@@ -49,30 +80,3 @@ export const Experience = () => {
 		</Box>
 	);
 };
-
-const skills = [
-	{
-		id: 0,
-		icon: <RiCustomerService2Fill />,
-		title: 'Customer Service',
-		text: 'I assist customers day-to-day by providing them with exceptional customer service.',
-	},
-	{
-		id: 1,
-		icon: <RiVideoAddLine />,
-		title: 'Digital Creator',
-		text: 'I create various digital works such as videos, designs, and artwork in Figma, Adobe Illustrator, Adobe Photoshop, and Adobe Premiere Pro.',
-	},
-	{
-		id: 2,
-		icon: <RiSoundModuleFill />,
-		title: 'Tech Crew',
-		text: 'I work with my fellow peers at school to create outstanding musical performances and events.',
-	},
-	{
-		id: 3,
-		icon: <RiTerminalBoxLine />,
-		title: 'Full-Stack Developer',
-		text: 'I regularly develop open-source projects using technologies such as Node.js, React, and TypeScript.',
-	},
-];
