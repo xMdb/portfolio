f8ad3547b899d0c0a61212c971146676ed50392c
Matt <46137770+xMdb@users.noreply.github.com>
2022-11-16 16:44:55 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
 	reactStrictMode: true,
+	async redirects() {
+    return [
+      {
+        source: '/nfc',
+        destination: 'https://linktr.ee/xmdbdev',
+        permanent: false,
+      },
+    ]
+  },
 };
